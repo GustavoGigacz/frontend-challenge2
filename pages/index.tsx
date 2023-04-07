4bcc370ac246f4369ccdfd2fd330b3fb585a17ff
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-04-07 17:20:27 -0300

    add mediaquery toi website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,56 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styled from "styled-components";
+
+import Inputs from "../components/shared/Inputs";
+import Button from "../components/shared/Button";
+import Display from "../components/shared/Display";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div></div>
+      <Section>
+        <Container>
+          <InputContainer>
+            <Inputs />
+            <Button />
+          </InputContainer>
+          <Display />
+        </Container>
+      </Section>
     </>
   );
 }
+
+const Section = styled.section`
+  min-height: 100%;
+  background-color: #f0f0f0;
+
+  display: grid;
+  place-items: center;
+`;
+
+const Container = styled.div`
+  width: 784px;
+  padding: 56px;
+
+  background-color: var(--white);
+  border-radius: 24px 24px 200px 24px;
+
+  @media (max-width: 1100px) {
+    width: 600px;
+  }
+
+  @media (max-width: 793px) {
+    width: 343px;
+  }
+`;
+
+// const CustomHr = styled.hr`
+//   height: 1px;
+//   border: none;
+//   background-color: rgba(220, 220, 220, 1);
+// `;
+
+const InputContainer = styled.form``;
---
+++ styles/variables.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const Variables = createGlobalStyle`
+  :root {
+
+  --white: #FFFFFF;
+
+  --light-grey: #716f6f;
+  --purple: #854DFF;
+  --greyblack: #151515;
+  
+  
+  }
+`;
+
+export default Variables;
